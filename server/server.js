fdf9c109db82cd5fee40b99f249f4518a62ead00
Akarawin Satho <Akwin.winny@gmail.com>
2017-10-05 13:26:23 +0700

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,15 @@ app.post('/todos',(req,res) => {
 	});
 });
 
+//Route GET
+app.get('/todos',(req,res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
+	},(e) => {
+		res.status(400).send(e);
+	})
+});
+
 
 app.listen(3000, () => {
 	console.log('Strat on port 300');
