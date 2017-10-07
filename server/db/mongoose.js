7ccb824d726cf98354f92e68db20f9ae6d0f8821
Akarawin Satho <Akwin.winny@gmail.com>
2017-10-08 00:05:28 +0700

    Add PATCH /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 let db ={
-	localhost: 'mongodb://localhost:27017/TodoApp',
+	// localhost: 'mongodb://localhost:27017/TodoApp',
 	mlab : 'mongodb://heroku_83r7z0qp:al8dqo2bhubjanufep8uupf87j@ds113785.mlab.com:13785/heroku_83r7z0qp'
 }
 mongoose.Promise = global.Promise;
