4bb77034e73f0e679dbb9cf1a191faf9563f5d45
Akarawin Satho <Akwin.winny@gmail.com>
2017-10-07 22:07:51 +0700

    new monggose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
-
+let db ={
+	localhost: 'mongodb://localhost:27017/TodoApp',
+	mlab: 'mongodb://akwins123:passwordis12@ds113505.mlab.com:13505/heroku_0lhms2pw'
+}
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(db.mlab || db.localhost);
 
 module.exports ={mongoose};
