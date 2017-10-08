c2fdc1c2290436df8c90041324ee4550205239de
Akarawin Satho <Akwin.winny@gmail.com>
2017-10-08 13:29:15 +0700

    add GET user/me and add authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -51,6 +51,24 @@ UserScema.methods.generateAuthToken = function() {
 		return token;
 	});
 };
+
+UserScema.statics.findByToken = function (token) {
+	var User = this;
+	var decoded;
+
+	try{
+		decoded =  jwt.verify(token,'abc132');
+	} catch (e) {
+
+		return Promise.reject();
+	}
+	return User.findOne({
+		_id: decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth'
+	});
+};
+
 var User = mongoose.model('User', UserScema);
 
 
